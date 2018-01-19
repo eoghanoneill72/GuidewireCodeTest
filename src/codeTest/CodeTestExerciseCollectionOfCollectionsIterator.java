9490e9aa20b6bbfc9e9d8216672495a3c34cba53
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2018-01-19 11:04:32 +0000

    second commit

--- src/codeTest/CodeTestExerciseCollectionOfCollectionsIterator.java
+++ src/codeTest/CodeTestExerciseCollectionOfCollectionsIterator.java
@@ -5,17 +5,24 @@ import java.util.Iterator;
 
 /**
  * Implement an iterator that iterates all the elements of a collection of collections
+ * @param <E>
  */
-public class CodeTestExerciseCollectionOfCollectionsIterator implements Iterator<Object> {
+public class CodeTestExerciseCollectionOfCollectionsIterator<E> implements Iterator<E> {
 
-	private Collection<Collection<Object>> _collOfColl = null;
+	private Collection<Collection<E>> _collOfColl = null;
+	
+	private Iterator<Collection<E>> outerIterator;
+	private Iterator<E> innerIterator;
+	private E next;
+	
 	
 	/**
 	 * Constructor takes in the collection of collections that should be iterated
 	 * @param collofColl collection of collections
 	 */
-	public CodeTestExerciseCollectionOfCollectionsIterator(Collection<Collection<Object>> collofColl) {
+	public CodeTestExerciseCollectionOfCollectionsIterator(Collection<Collection<E>> collofColl) {
 		_collOfColl = collofColl;
+		outerIterator = _collOfColl.iterator();	
 	}
 	
 	/**
@@ -23,13 +30,20 @@ public class CodeTestExerciseCollectionOfCollectionsIterator implements Iterator
 	 */
 	public boolean hasNext() {
 		// TODO implement this method
+		if(outerIterator.hasNext()) {
+			innerIterator = outerIterator.next().iterator();
+			if(innerIterator != null || innerIterator.hasNext()) {
+				next = innerIterator.next();
+				return true;
+			}
+		}
 		return false;
 	}
 
 	/**
 	 * Returns the next element in the iteration
 	 */
-	public Object next() {
+	public E next() {
 		// TODO implement this method
 		return null;
 	}
